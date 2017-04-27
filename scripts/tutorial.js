454ed1725697b7ae3040a3ef73067a4895ab2805
Jason Chen <jasonccmfry@gmail.com>
2017-04-26 22:58:54 -0700

    New Cover Page

--- scripts/tutorial.js
+++ scripts/tutorial.js
@@ -1,3 +1,9 @@
+/* jQuery functions */
+$(document).ready(function() {
+    $("#banner-text p").delay(500).animate({"opacity": "1"}, 700);
+    $("#banner-text h1").delay(250).animate({"opacity": "1"}, 700);
+});
+
 function textChange() {
   document.getElementById("paragraph").innerHTML = "Hello World";
-} 
+}
