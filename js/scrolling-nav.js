6f0a702eb42fb9d4ca5303ce78a8ba84004eaa8a
Jason Chen <jasonccmfry@gmail.com>
2017-06-08 18:21:17 -0700

    Modify animation when DOM is ready

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -11,14 +11,14 @@ $(document).ready(function() {
       $('.fixed-nav').fadeOut(500);
     }
   });
-});
 
-$(function() {
+  $(function() {
     $(document).on('click', '.scroll', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1200, 'easeInOutExpo');
-        event.preventDefault();
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+      }, 1200, 'easeInOutExpo');
+      event.preventDefault();
     });
+  });
 });
